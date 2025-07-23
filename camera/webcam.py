5524dd788796a0973ac8e362478b835ffa81c5df
todor <krastev.t@gmail.com>
2025-07-23 19:31:11 +0200

    Implement proper saving.

--- camera/webcam.py
+++ camera/webcam.py
@@ -89,8 +89,8 @@ class WEBCAM(Thread):
             sorted_buffer=sorted(((abs(ts - time_stamp), frame) for ts, frame in self.__image_buffer), key=lambda x: x[0])
             
         for frame in sorted_buffer[0:image_count]:
-            frame_name=os.path.join(output_path,str(time)+".jpg")
-            cv2.imwrite(filename=frame_name,img=frame)
+            frame_name=os.path.join(output_path,str(time())+".jpg")
+            cv2.imwrite(frame_name,frame[1])
         
         
             
