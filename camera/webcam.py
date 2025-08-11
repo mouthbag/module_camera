1a52393d7f305f63e5a6a555bc9b6f9b343b0fbb
Todor Krastev <krastev.t@gmail.com>
2025-08-11 07:34:48 +0100

    Disable color conversion.

--- camera/webcam.py
+++ camera/webcam.py
@@ -61,7 +61,7 @@ class WEBCAM(Thread):
             
             ret, frame = self.__camera.read()
             
-            frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            #frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
             
             if not ret:
                 self.__logger.warning("Could not read frame.")
